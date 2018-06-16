c891e8b936d2abc14064eaf08b63290318dd3d97
emabimusik <emabi2015@gmail.com>
2018-06-16 09:38:37 +0100

    first commit

---
+++ BookRental/Controllers/CatalogController.cs
@@ -0,0 +1,41 @@
+﻿using Library.Models.Catalog;
+using LibraryServices;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class CatalogController:Controller
+    {
+        private ILibraryAsset _asset;
+        public CatalogController(ILibraryAsset asset)
+        {
+            _asset = asset;
+        }
+        public IActionResult Index()
+        {
+            var assetModels = _asset.GetAll();
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _asset.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _asset.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _asset.GetType(result.Id)
+
+                });
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ LibraryData/Migrations/20180406140629_TelphoneMumberAddedmigration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryData.Migrations
+{
+    public partial class TelphoneMumberAddedmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TelephoneNumber",
+                table: "Patrons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TelephoneNumber",
+                table: "Patrons");
+        }
+    }
+}
